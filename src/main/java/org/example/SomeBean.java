3cbef48ff76b9487db1283f61cbbfbffb1bf96a7
NezKap <werras206@mail.ru>
2025-12-24 23:05:27 +0300

    Добавление Java Doc в класс SomeBean

--- src/main/java/org/example/SomeBean.java
+++ src/main/java/org/example/SomeBean.java
@@ -1,10 +1,19 @@
 package org.example;
 
+/**
+ * Класс с объявленными полями
+ * @author Егор
+ * @version 1.0
+ */
 class SomeBean {
     @AutoInjectable
     private SomeInterface field1;
     @AutoInjectable
     private SomeOtherInterface field2;
+
+    /**
+     * Метод, вызывающий методы у полей
+     */
 
     public void foo() {
         field1.doSomething();
