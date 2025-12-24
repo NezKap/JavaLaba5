1833a629b66ee46e81fbf2a3e670b08de16edcd8
NezKap <werras206@mail.ru>
2025-12-24 23:03:09 +0300

    Добавление Java Doc в аннотацию AutoInjectable

--- src/main/java/org/example/AutoInjectable.java
+++ src/main/java/org/example/AutoInjectable.java
@@ -2,9 +2,16 @@ package org.example;
 
 import java.lang.annotation.*;
 
+/**
+ * Аннотация для полей, которые нужно инициализировать
+ * @author Егор
+ * @version 1.0
+ */
+
 @Retention(RetentionPolicy.RUNTIME)
 
 @Target(ElementType.FIELD)
+
 
 public @interface AutoInjectable {
 
